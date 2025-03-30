1a76dc9bb1b265657401373134b847e563f84b5c
caydeny <caydenyoo@hotmail.com>
2025-03-29 23:03:02 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: './',
-  build: {
-    outDir: 'dist',
-  },
-});
+  plugins: [react()],
+  base: "/react-vite/",
+})
